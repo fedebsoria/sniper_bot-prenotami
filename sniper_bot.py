47db20ea1a413850ced2eb979c71aececf574846
unknown <fsoria7@outlook.com>
2023-01-13 17:25:56 +0100

    solve all the bugs with the buttons

--- sniper_bot.py
+++ sniper_bot.py
@@ -15,10 +15,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-  #type: ignorefrom pyautogui import screenshot
-
 starting_web_browser = "Abriendo el navegador. POR FAVOR NO CERRAR hasta que se haya hecho el screenshot\n"
-user_credentials = []
 
 #layout for the UI
 sg.theme('DarkAmber')
@@ -31,21 +28,28 @@ layout = [ [sg.Text("Sniper bot para prenotami."),],
         ]
 
 
-#check if there're no user_credentials
-def create_user_credentials(user_credentials):
-    if file_exist("user_credentials_file") != True:
-        with open("user_credentials_file", "wb") as ud:
+#check if there're no user_credentials and create it
+def create_user_credentials_file(user_file):
+    if not file_exist(user_file):
+        with open(user_file, "wb") as ob:
+            pickle.dump([], ob)
+    else:
+        pass
+
+def write_user_credentials(user_credentials, user_file):
+    if file_exist(user_file) != True:
+        with open(user_file, "wb") as ud:
             pickle.dump(user_credentials, ud)
     else:
          pass
 
-def read_user_credentials():
+def read_user_credentials(user_file, user_credentials):
     try:
-        with open("user_credentials_file", "rb") as uc:
+        with open(user_file, "rb") as uc:
             user_credentials = pickle.load(uc)
-            return user_credentials
     except:
         pass
+    return user_credentials
 
 
 #takes screenshots
@@ -73,9 +77,9 @@ def web_driver_sign_in(url, u_email, u_password, driver, window):
     #log_in_error = driver.find_element("id", "login-password-error")
     try:
         prenota = driver.find_element("id", "advanced").click()
-        sleep(3)
+        sleep(5)
         cittadinanza_per_discendenza = driver.find_element(By.XPATH, "//a[contains(@href, '/Services/Booking/224')]").click()
-        sleep(3)
+        sleep(5)
         no_appointment = None
         no_appointment = driver.find_element(By.XPATH, "//*[contains(text(),'Al momento non ci sono date disponibili per il servizio richiesto')]")
         try:
@@ -84,22 +88,35 @@ def web_driver_sign_in(url, u_email, u_password, driver, window):
             window.Element("-display-").print("Algo salió mal o hay turno para el tramite.\n Seguro que algo salió mal.\n Cerrar y volver a correr.\n")  
     except Exception:
         window.Element("-display-").print("Usuario o contraseña incorrectos")
-        os.remove("user_credentials_file")
+        #os.remove("user_credentials_file")
         window.Element("-start-").update(disabled=False)
         window.Element("-user_email-").Update(disabled=False)
         window.Element("-user_password-").Update(disabled=False)
 
 def main():
     
-    window = sg.Window("Prenotami", layout)
-
-    user_file = file_exist("user_credentials_file")
+    user_file = "user_credentials_file"
     user_file_path = "./user_credentials_file"
-
     #user is store in [0] and password is store in [1]
     user_credentials = []
+    u_email = ""
+    u_password = ""
+
+    #create_user_credentials_file(user_file)
+    
+    
+    user_credentials = read_user_credentials(user_file, user_credentials)
+    if user_credentials != []:                        
+            #the user is the e-mail and is split here:
+        u_email = user_credentials[0]
+        u_password = user_credentials[1]
+    else:
+        write_user_credentials(user_credentials, user_file)
 
 
+
+    window = sg.Window("Prenotami", layout)
+    
     while True:
         #close the window and stop the script
         event, values = window.read()        
@@ -107,63 +124,44 @@ def main():
             sys.exit()
             
         #check if there's ./user_credentials_file
-        while user_file == True:
-            #read the file
-            user_credentials = read_user_credentials()
-                        
-            #the user is the e-mail and is split here:
-            u_email = user_credentials[0]
-            u_password = user_credentials[1]
+
             
   
-            #erase the ./user_credentials_file
-            if event == "-Erase-":
-
-                os.remove(user_file_path)
-
-                window.Element("-display-").print("Datos de usuario borrados\n")
-                pass
+            #erase the ./user_credentials_file.bin
+        if event == "-Erase-":
+            os.remove(user_file)
+            window.Element("-display-").print("Datos de usuario borrados\n")
+            pass
            
              #read the user_credentials_file in a list
-
-            if event == "-l_user-":
-                window.Element("-display-").print("Hay datos guardados\n")        
-                window.Element("-user_email-").update(value=u_email)
-                window.Element("-user_password-").update(value=u_password)
-                window.Element("-display-").print("Datos obtenidos.\n")
-                break
-
-               
-        else:
-
-            u_email = values["-user_email-"]
-            u_password = values["-user_password-"]
-            user_credentials.append(u_email)
-            user_credentials.append(u_password)
-            create_user_credentials(user_credentials)
-        
-             
-
-
-
-        #check if exist user_credentials_file and if it doesn't ask for the credentials
-        #the password is at least 8 characters and is store here:
+        if event == "-l_user-":
+            user_credentials = read_user_credentials(user_file, user_credentials)
+            u_email = user_credentials[0]
+            u_password = user_credentials[1]
+            window.Element("-display-").print("Hay datos guardados\n")        
+            window.Element("-user_email-").update(value=u_email)
+            window.Element("-user_password-").update(value=u_password)
+            window.Element("-display-").print("Datos obtenidos.\n")
             
-
         if event == "-start-":
-            window.Element("-start-").update(disabled=True)
-            window.Element("-user_email-").Update(disabled=True)
-            window.Element("-user_password-").Update(disabled=True)
-            window.Element("-display-").print("Empezando...\n")
+                u_email = values["-user_email-"]
+                u_password = values["-user_password-"]
+                user_credentials.append(u_email)
+                user_credentials.append(u_password)
+                write_user_credentials(user_credentials, user_file)
+                window.Element("-start-").update(disabled=True)
+                window.Element("-user_email-").Update(disabled=True)
+                window.Element("-user_password-").Update(disabled=True)
+                window.Element("-display-").print("Empezando...\n")
 
-            window.Element("-display-").print(starting_web_browser)
+                window.Element("-display-").print(starting_web_browser)
 
-            url = "https://prenotami.esteri.it/"
-            driver = webdriver.Firefox()
+                url = "https://prenotami.esteri.it/"
+                driver = webdriver.Firefox()
 
-            web_driver_sign_in(url, u_email, u_password, driver, window)
+                web_driver_sign_in(url, u_email, u_password, driver, window)
 
-            driver.close()
+                driver.close()
     
 
 if __name__ == "__main__":
